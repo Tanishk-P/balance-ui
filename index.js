6fc0811e2a617a54106eb1b8009c17db4f0e6713
Tanishk <tanishkparadkar@gmail.com>
2024-12-28 23:18:55 +0530

    app name registration

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Platform } from 'react-native';
 import { startNetworkLogging } from 'react-native-network-logger';
 import { App } from './App';
 import { name as appName } from './app.json';
 
+const _appName = Platform.OS === "ios" ? appName : "balanceui"
 startNetworkLogging();
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(_appName, () => App);
